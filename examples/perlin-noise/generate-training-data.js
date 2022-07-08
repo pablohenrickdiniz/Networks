6145d88531b8ba638cdee4de3d05624381870057
Pablo Henrick Diniz <phydokz@hotmail.com>
2022-07-08 08:50:46 -0400

    correção geração de dados

--- examples/perlin-noise/generate-training-data.js
+++ examples/perlin-noise/generate-training-data.js
@@ -7,32 +7,50 @@ function randIndex(max){
     return Math.round(Math.random()*max*sign);
 }
 
+function max_values(){
+    let iterations = 5;
+    let max_it = null;
+    let max_scale = null;
+    let scale = 0;
+    while(scale < max){
+        max_it = iterations;
+        max_scale = scale;
+        iterations++;
+        scale = Math.pow(2,iterations-1)*7.8125;
+    }
+    return {
+        iterations: max_it,
+        scale: max_scale  
+    };
+}
+
+
 module.exports = function(num_examples){
     let examples = [];
     let existing = [];
-    
-    for(let i = 0; i < 100;i++){
-        let iterations = 5 + Math.floor(Math.random()*50);
+    let max_conf = max_values();
+    while(examples.length < num_examples){
+        let iterations = 5 + Math.floor(Math.random()*(max_conf.iterations-5));
         let seed = Math.round(Math.random()*max*(Math.round(Math.random()) === 1?1:-1));
         let scale = Math.pow(2,iterations-1)*7.8125;
+        if(scale > max_conf.scale){
+            continue;
+        }
         let map = new HeightMap({
             seed:seed,
             iterations:iterations,
             scale:scale
         });
-
-        while(examples.length < num_examples){
-            let i = randIndex(max);
-            let j = randIndex(max);
-            let example = {
-                input:[seed,scale,i,j],
-                output:map.get(i,j)
-            };
-            let hash = stringHash(JSON.stringify(example));
-            if(existing.indexOf(hash) === -1){
-                existing.push(hash);
-                examples.push(example);
-            }
+        let i = randIndex(max);
+        let j = randIndex(max);
+        let example = {
+            input:[seed,scale,i,j],
+            output:map.get(i,j)
+        };
+        let hash = stringHash(JSON.stringify(example));
+        if(existing.indexOf(hash) === -1){
+            existing.push(hash);
+            examples.push(example);
         }
     }
     return examples;
