5168599a288a6ccfe1bb0850458cc4ff012488e9
Xploit <xploit@mailfence.com>
2022-07-14 14:43:45 -0400

    skip predicted

--- examples/resolution/generate.js
+++ examples/resolution/generate.js
@@ -13,6 +13,9 @@ const outputsDir = '/content/drive/MyDrive/ia-projects/resolution/outputs';
     for(let i = 0; i < dirs.length;i++){
         let modelDir = dirs[i];
         let outputDir = path.join(outputsDir,path.basename(modelDir));
+        if(fs.existsSync(outputDir)){
+            continue;
+        }
         await predict(modelDir,imagesDir,outputDir);
     }
 })();
