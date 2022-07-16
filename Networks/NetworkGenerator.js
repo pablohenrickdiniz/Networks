3ae7377f1d2b26b5b77c86b4cd3a14520918a43b
Pablo Henrick Diniz <phydokz@hotmail.com>
2022-07-16 11:08:33 -0400

    network

--- Networks/NetworkGenerator.js
+++ Networks/NetworkGenerator.js
@@ -1,27 +1,44 @@
 const optimizers = ['adadelta', 'adam', 'adamax', 'aldagrad', 'momentum', 'rmsprop', 'sgd'];
 const activations = ['elu', 'hardSigmoid', 'linear', 'mish', 'relu', 'relu6', 'selu', 'sigmoid', 'softmax', 'softplus', 'softsign', 'swish', 'tanh'];
 const layers = ['conv2d', 'dense', 'dropout', 'elu', 'flatten', 'gru', 'layerNormalization', 'leakyReLU', 'lstm', 'maxPooling2d', 'prelu', 'reLU', 'reshape', 'simpleRNN', 'softmax', 'thresholdedReLU', 'upSampling2d'];
-const numericFields = ['filters','units'];
-const losses = [];
+const losses = [
+	'absoluteDifference',
+	'computeWeightedLoss', 
+	'cosineDistance', 
+	'hingeLoss', 
+	'huberLoss', 
+	'logLoss', 
+	'meanSquaredError', 
+	'sigmoidCrossEntropy', 
+	'softmaxCrossEntropy'
+];
 
 function parseOptimizers(text){
-	return parseList(text,optimizers);
+	return parseList(text,optimizers,'sgd');
 }
 
 function parseActivations(text){
-	return parseList(text,activations);
+	return parseList(text,activations,'linear');
 }
 
 function parseLayersTypes(text){
-	return parseList(text,layers);
+	return parseList(text,layers,'dense');
 }
 
-function parseList(text,list){
+function parseLosses(text){
+	return parseList(text,losses,'meanSquaredError');
+}
+
+function parseList(text,list,def){
 	text = String(text).split('|');
 	if(text.indexOf('*') !== -1){
 		return [...list].sort();
 	}
-    return [...new Set(text.filter((o) => list.indexOf(o) !== -1).sort())];
+    list =  [...new Set(text.filter((o) => list.indexOf(o) !== -1).sort())];
+	if(list.length === 0){
+		list = [def];
+	}
+	return list;
 }
 
 function parseNumeric(text){
@@ -54,44 +71,46 @@ function parseNumeric(text){
 	return numbers.sort((a,b) => a-b);
 }
 
-function parseShape(shape){
-	shape = [...new Set(shape)];
-	let shapes = [];
-
-	if(shape.length > 0){
-		let tmp = [];
-		let indexes = [];
-		for(let i = 0; i < shape.length;i++){
-			let numbers = parseNumeric(shape[i]);
-			tmp.push(numbers);
-			indexes.push(0);
+function iterateOverPermutations(groups,callback){
+	let indexes = [];
+	for(let i = 0; i < groups.length;i++){
+		indexes.push(0);
+	}
+	let total = groups.reduce(function(a,b){
+		return a * b.length;
+	},1);
+	let last = indexes.length - 1;
+	for(let i = 0; i < total;i++){
+		let perm = [];
+		for(let j = 0; j < indexes.length;j++){
+			perm.push(groups[j][indexes[j]]);
 		}
-
-		let total = tmp.reduce(function(a,b){
-			return a * b.length;
-		},1);
-
-		let last = indexes.length - 1;
-		
-		for(let i = 0; i < total;i++){
-			let shape = [];
-			for(let j = 0; j < indexes.length;j++){
-				shape.push(tmp[indexes[j]]);
-			}
-			shapes.push(shape);
-			indexes[indexes.length-1]++;
-			for(let j = last; j >= 0; j--){
-				if(indexes[j] >= tmp[j].length){
-					indexes[j] = 0;
-					let prev = j-1;
-					if(prev >= 0){
-						indexes[prev]++;
-					}
+		callback(perm,i);
+		indexes[indexes.length-1]++;
+		for(let j = last; j >= 0; j--){
+			if(indexes[j] >= groups[j].length){
+				indexes[j] = 0;
+				let prev = j-1;
+				if(prev >= 0){
+					indexes[prev]++;
 				}
 			}
 		}
 	}
+}
 
+function parseShape(shape){
+	let shapes = [];
+	if(shape && shape.constructor === [].constructor){
+		let tmp = [];
+		for(let i = 0; i < shape.length;i++){
+			let numbers = parseNumeric(shape[i]);
+			tmp.push(numbers);
+		}
+		iterateOverPermutations(tmp,function(perm,index){
+			shapes.push(perm);
+		});
+	}
 	return shapes;
 }
 
@@ -124,7 +143,7 @@ function parseLayers(_layers){
           	targetShapes = parseShape(l.targetShape);
 		}
 
-		lrs.push({
+		let _layer = {
 			types: types,
 			activations: activations,
 			units: units,
@@ -134,21 +153,57 @@ function parseLayers(_layers){
 			strides: strides,
 			rates: rates,
 			targetShapes: targetShapes
-		});
-	}
+		};
 
+		Object.keys(_layer).forEach(function(k){
+			if(_layer[k].length === 0){
+				delete _layer[k];
+			}
+		});
+		
+		lrs.push(_layer);
+	}
+	
 	return lrs;
 }
 
 let NetworkGenerator  = function(config){
-   let self = this;
-   self.data = {
-   		optimizer: parseOptimizers(config.optimizer),
-   		loss: parseLosses(config.loss),
-   		layers: parseLayers(config.layers)
-   };	
+    initialize(this,{
+		optimizers: parseOptimizers(config.optimizer),
+		losses: parseLosses(config.loss),
+		layers: parseLayers(config.layers)
+	});
 };
 
+function initialize(self,data){
+	let length = null;
+
+	let getItem = function(index){
+
+	};
+
+	Object.defineProperty(self,'length',{
+		get:function(){
+			if(length === null){
+				length = data.optimizers.length*data.losses.length*data.layers.reduce(function(a,b){
+					let keys = Object.keys(b);
+					let prod = 1;
+					for(let i = 0; i < keys.length;i++){
+						prod *= b[keys[i]].length;
+					}
+					return a*prod;
+				},1);
+			}	
+			return length;	
+		}
+	});
+
+	Object.defineProperty(self,'getItem',{
+		get:function(){
+			return getItem;
+		}
+	});
+}
 
 
 
