cb22051cd456c87bf2145a12903b4a47317d2176
Xploit <xploit@mailfence.com>
2022-07-18 16:37:37 -0400

    upload

--- examples/resolution/train.js
+++ examples/resolution/train.js
@@ -11,7 +11,7 @@ const predict = require('./predict');
 const epochs = 500;
 const imagesDir = '/content/drive/MyDrive/ia-projects/resolution/images';
 const outputsDir = '/content/drive/MyDrive/ia-projects/resolution/outputs';
-const top = require('./top');
+//const top = require('./top');
 
 async function train(){
     if(!fs.existsSync(highResDir)){
@@ -54,21 +54,37 @@ async function train(){
         inputShape:[128,128,3],
         outputShape:[2048,2048,3],
         layers:[
-            {type:'conv2d',filters:'2|4|8|16|32|64',activation:'*'},
-            {type:'conv2d',filters:3,activation:'*'},
+            {type:'conv2d',filters:'2|4|8|16|32|64',activation:'elu|relu|selu|relu6'},
+            {type:'conv2d',filters:3,activation:'elu|relu|selu|relu6'},
             {type:'upSampling2d',size:[16,16]}
         ],
         optimizer:'rmsprop|adam|sgd',
         loss:'meanSquaredError|absoluteDifference'
     });
+
+     //config C
+     configs = new NetworkGenerator({
+        inputShape:[128,128,3],
+        outputShape:[2048,2048,3],
+        layers:[
+            {type:'conv2d',filters:'64',activation:'elu|relu|selu'},
+            {type:'conv2d',filters:3,activation:'elu|relu|selu'},
+            {type:'upSampling2d',size:[16,16]}
+        ],
+        optimizer:'adam|sgd',
+        loss:'meanSquaredError|absoluteDifference'
+    });
+
     for(let i = 0; i < configs.length;i++){
         let random = Math.floor(Math.random()*configs.length);
-       // let random = i;
         let config = configs.getItem(random);
       
         let id = String(stringHash(JSON.stringify(config)));
         let modelDir = path.join(modelsDir,id);
+        console.log(`${i+1}/${configs.length} - processando modelo ${id}...`);
+        console.log(JSON.stringify(config));
         if(fs.existsSync(modelDir)){
+            console.log(`${i+1}/${configs.length} - pulando modelo ${id}...`);
             continue;
         }
      
@@ -105,7 +121,7 @@ async function train(){
         
         await net.save(modelDir);
         await predict(modelDir,imagesDir,outputsDir);
-        await top(1000,100);
+        //await top(1000,100);
     }
 };
 
